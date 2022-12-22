0c0802c8eb5c3c1e296a76812dc3d9027bc5a917
ponciks <mehmeteminselek@gmail.com>
2022-12-22 15:22:37 +0300

    docker

--- REG001PREREG/settings.py
+++ REG001PREREG/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'REG001PREREG.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'db',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '8080',
     }
 }
 
